31aefbe4a969e325b0c3cb459d14459f7f0aab03
Luke Hanna <Luke.m.hanna@gmail.com>
2024-02-15 19:59:43 -0500

    Added additional processing to the GPS function

--- van/vanhub.py
+++ van/vanhub.py
@@ -23,18 +23,37 @@ def get_gps_data():
         formatting = sentence[1:6]
         values = sentence.split(',')
         if formatting == 'GPGGA':
-            data['time'] = values[1]
-            data['latitude'] = (values[2], values[3])
-            data['longitude'] = (values[4], values[5])
-            data['fix_quality'] = values[6]
-            data['satellites_used'] = values[7]
+            data['utc_time'] = float(values[1])
+
+            # Process N/S/E/W into decimal lat/long
+            latitude = float(values[2])
+            if values[3] == 'W':
+                latitude = latitude * -1
+            longitude = float(values[4])
+            if values[5] == 'S':
+                longitude = longitude * -1
+            data['latitude'] = latitude
+            data['longitude'] = longitude
+
+            # Fix quality processing
+            fq = values[6]
+            if fq == 0:
+                fq = 'Not Fixed'
+            elif fq == 1:
+                fq = 'GPS Fix'
+            elif fq == 2:
+                fq = 'DGPS Fix'
+            else:
+                fq = f'Unknown Fix: {fq}'
+            data['fix_quality'] = fq
+            data['satellites_used'] = int(values[7])
             data['HDOP'] = values[8]
-            data['altitude'] = (values[9], values[10])
+            data['altitude'] = float(values[9])
             found['GPGGA'] = True
         if formatting == 'GPVTG':
             data['true_track'] = values[1]
             data['magnetic_track'] = values[3]
-            data['ground_speed'] = values[7]
+            data['ground_speed'] = float(values[7])
             found['GPVTG'] = True
         if all(value == True for value in found.values()):
             return data
