606089b05ef0d338dbf4e74a4dfa1857ca1996f4
Luke Hanna <Luke.m.hanna@gmail.com>
2024-02-15 19:40:07 -0500

    Added a function to get gps data

--- van/vanhub.py
+++ van/vanhub.py
@@ -1,8 +1,43 @@
 import argparse
+import json
+
+import serial
+
+gps = serial.Serial('/dev/ttyACM0', baudrate=9600)
 
 
 def get_gps_data():
-    return 'hello'
+    formats = ['GPGGA', 'GPVTG']
+    found = {format_type: False for format_type in formats}
+    data = {}
+
+    # Flush buffer
+    gps.flushInput()
+    gps.flushOutput()
+
+    # Trash the first line because it might be malformed
+    _ = gps.readline()
+    while True:
+        parsed = []
+        sentence = gps.readline().decode('utf-8')
+        formatting = sentence[1:6]
+        values = sentence.split(',')
+        if formatting == 'GPGGA':
+            data['time'] = values[1]
+            data['latitude'] = (values[2], values[3])
+            data['longitude'] = (values[4], values[5])
+            data['fix_quality'] = values[6]
+            data['satellites_used'] = values[7]
+            data['HDOP'] = values[8]
+            data['altitude'] = (values[9], values[10])
+            found['GPGGA'] = True
+        if formatting == 'GPVTG':
+            data['true_track'] = values[1]
+            data['magnetic_track'] = values[3]
+            data['ground_speed'] = values[7]
+            found['GPVTG'] = True
+        if all(value == True for value in found.values()):
+            return data
 
 
 parser = argparse.ArgumentParser()
@@ -12,6 +47,6 @@ args = parser.parse_args()
 
 if args.action == 'sensor':
     if args.sensor == 'gps':
-        print(get_gps_data())
+        print(json.dumps(get_gps_data(), indent=4))
     elif args.sensor == 'tio':
         print('weather')
