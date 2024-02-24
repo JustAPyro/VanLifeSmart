b0d1774395e3c093937b3a801d0c773d21fb3e86
Luke Hanna <Luke.m.hanna@gmail.com>
2024-02-24 13:17:19 -0500

    Fixed bug with url prefixing

--- van/scheduler.py
+++ van/scheduler.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-schedule_urls = APIRouter(prefix='schedule')
+schedule_urls = APIRouter(prefix='/schedule')
 
 
-@schedule_urls.get("all.json")
+@schedule_urls.get(".json")
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
