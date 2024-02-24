270ff18549526db19c2b3395583048fd2cfe7e7e
Luke Hanna <Luke.m.hanna@gmail.com>
2024-02-24 13:15:37 -0500

    Fixed small url collision

--- van/scheduler.py
+++ van/scheduler.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 schedule_urls = APIRouter(prefix='schedule')
 
 
-@schedule_urls.get(".json")
+@schedule_urls.get("all.json")
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
