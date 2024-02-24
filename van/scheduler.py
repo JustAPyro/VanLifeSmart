047d552984ae702b0a7448447f548dbdf82c3504
Luke Hanna <Luke.m.hanna@gmail.com>
2024-02-24 13:36:32 -0500

    /schedule.json should now return all jobs

--- van/scheduler.py
+++ van/scheduler.py
@@ -1,18 +1,11 @@
+from apscheduler.job import Job
 from fastapi import APIRouter
-
-schedule_urls = APIRouter(prefix='/schedule')
-from sensors import sensors
 from local_server import scheduler
 
+schedule_urls = APIRouter(prefix='/schedule')
 
-def schedule_info(job_id: str):
-    job = scheduler.get_job(job_id)
-    if not job:
-        return {'error': 'could not find job', 'jobs': [{
-            'id': job.id,
-            'description': job.name
-        } for job in scheduler.get_jobs()]}
 
+def schedule_info(job: Job):
     return {
         'description': job.name,
         'trigger': str(job.trigger),
@@ -24,5 +17,4 @@ def schedule_info(job_id: str):
 
 @schedule_urls.get(".json")
 async def read_users():
-    output = {'report': schedule_info('report')}
-    return output
+    return [schedule_info(job) for job in scheduler.get_jobs()]
