61ce61c9f1ee2e94068f5afdf5edf0bb4ac56041
Luke Hanna <Luke.m.hanna@gmail.com>
2024-02-26 23:17:42 -0500

    Added log delete file

--- van/logs/endpoints.py
+++ van/logs/endpoints.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request
+import os
+from fastapi import APIRouter, Request, Response
 
 log_urls = APIRouter(prefix='/logs')
 
@@ -6,3 +7,13 @@ log_urls = APIRouter(prefix='/logs')
 @log_urls.get('/hi')
 async def schedule_page(request: Request):
     return 'hello'
+
+
+@log_urls.delete('/{log_name}.json')
+async def delete_log(log_name: str):
+    # TODO: AUTHENTICATION VALIDATION
+    try:
+        os.remove(f'{os.getenv("VLS_LOCATION")}/van/logs/{log_name}')
+        return Response(status_code=200)
+    except OSError:
+        return Response(status_code=400)
