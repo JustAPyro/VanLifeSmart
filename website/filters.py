18160fcea1f08bd25a4a367e1d1b9c80595c44e1
JustAPyro <luke.m.hanna@gmail.com>
2024-04-12 19:17:21 -0400

    Cleaned up the home page of server

--- website/filters.py
+++ website/filters.py
@@ -2,6 +2,8 @@ import pytz
 from pytz import timezone
 
 def datetimefilter(value, format='%I:%M:%S %p'):
+    if value is None:
+        return ''
     tz = pytz.timezone('US/Eastern') # timezone you want to convert to from UTC
     utc = pytz.timezone('UTC')  
     value_adj = utc.localize(value, is_dst=None).astimezone(pytz.utc)
