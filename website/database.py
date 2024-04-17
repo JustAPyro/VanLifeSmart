b834059e0a5ce19a40065b888d7344b58436ce7e
JustAPyro <luke.m.hanna@gmail.com>
2024-04-17 10:32:59 -0400

    Fixed typo for pitstop page

--- website/database.py
+++ website/database.py
@@ -4,8 +4,4 @@ from flask_sqlalchemy import SQLAlchemy
 from models import Base
 
 load_dotenv()
-db = SQLAlchemy(model_class=Base, engine_options={
-    'connect_args': {
-        'connect_timeout': 10
-    }
-})
+db = SQLAlchemy(model_class=Base)
